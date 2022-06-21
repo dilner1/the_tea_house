5ccf3fe75fbb791745b92338beeb49bb7f06fbc3
dilner1 <dan.milner871@gmail.com>
2022-06-21 19:01:38 +0000

    updating ALLOWED_HOSTS as not working

--- the_tea_house/settings.py
+++ the_tea_house/settings.py
@@ -78,9 +78,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'the_tea_house.wsgi.application'
 
 if development:
-    ALLOWED_HOSTS = ['localhost']
+    ALLOWED_HOSTS = ['localhost', 'the-tea-house-p5.herokuapp.com']
 else:
-    ALLOWED_HOSTS = [os.environ.get('the-tea-house-p5.herokuapp.com')]
+    ALLOWED_HOSTS = [os.environ.get('https://the-tea-house-p5.herokuapp.com/')]
+
+    # add HEROKU_HOSTNAME
 
 
 AUTHENTICATION_BACKENDS = [
