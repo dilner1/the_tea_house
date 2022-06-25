bc44c448a9cdded9569434385d5477e212daa17c
dilner1 <dan.milner871@gmail.com>
2022-06-25 10:48:03 +0000

    Adding new html pages and creating url paths

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views 
+
 from . import views
 urlpatterns = [
     path('', views.index, name='Home'),
-]
- 
+    path('tea-store/', views.teaStore, name='teaStore'),
+    path('pots-store/', views.potsStore, name='potsStore'),
+    path('sets-store/', views.setsStore, name='setsStore'),
+    path('basket/', views.basket, name='basket'),
+    path('checkout/', views.checkout, name='checkout'),
+    ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,23 @@ from django.shortcuts import render
 def index(request):
     """ Loads index page """
     return render(request, 'app/index.html')
+
+def teaStore(request):
+    context = {}
+    return render(request, 'app/tea-store.html', context)
+
+def potsStore(request):
+    context = {}
+    return render(request, 'app/posts-store.html', context)
+
+def setsStore(request):
+    context = {}
+    return render(request, 'app/sets-store.html', context)
+
+def basket(request):
+    context = {}
+    return render(request, 'app/basket.html', context)
+
+def checkout(request):
+    context = {}
+    return render(request, 'app/checkout.html', context)
