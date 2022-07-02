8aa6750f723aaabd3d0a9731eabefb29b05565ce
dilner1 <dan.milner871@gmail.com>
2022-07-02 06:40:05 +0000

    Reverting BASE_DIR

--- the_tea_house/settings.py
+++ the_tea_house/settings.py
@@ -17,8 +17,7 @@ if os.path.isfile('env.py'):
     import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 development = os.environ.get('DEVELOPMENT', False)
 
