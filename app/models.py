3411dfb4bebf2aca78edc87a1895fc5b1f168d02
dilner1 <dan.milner871@gmail.com>
2022-07-12 19:47:32 +0000

    Adding user model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserDetails(models.Model):
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=odels.CASCADE)
+    f_name = models.CharField(max_length=30, null=True)
+    l_name = models.CharField(max_length=30, null=True)
+    email = models.EmailField()
