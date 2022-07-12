2098f36598c7cbea2a247f4d01f0b6eb24dd4047
dilner1 <dan.milner871@gmail.com>
2022-07-12 20:14:02 +0000

    Adding product model

--- app/models.py
+++ app/models.py
@@ -9,6 +9,10 @@ class UserDetails(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=50, null=True)
+    sku = models.CharField(max_length=254, null=True, blank=True)
     price = models.FloatField()
     info = models.TextField()
     image = models.ImageField()
+    
+    def __str__(self):
+        return self.name
