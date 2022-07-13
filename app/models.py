d5bf170e968fbd0798f8ae14340653199fb11ee1
dilner1 <dan.milner871@gmail.com>
2022-07-13 20:08:14 +0000

    Adding delivery address class

--- app/models.py
+++ app/models.py
@@ -26,8 +26,24 @@ class Basket(models.Model):
     def __str__(self):
         return str(self.order_number)
 
-class OrderItems(models.Model):
+class BasketOrderItems(models.Model):
     item = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
     order = models.ForeignKey(Basket, on_delete=models.SET_NULL, blank=True, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     added_date = models.DateTimeField(auto_now_add=True,)
+
+class DeliveryAddress(models.Model):
+    userDetail = models.ForeignKey(UserDetail, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Basket, on_delete=models.SET_NULL, blank=True, null=True)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    info = models.TextField(null=True, blank=True)
+
+    
+    
+    
