bc5dce7b0b1b9617d2fedcaf73510c9a8e1bd311
dilner1 <dan.milner871@gmail.com>
2022-07-14 18:23:46 +0000

    registering models in admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import UserDetails, Product, Basket, BasketOrderItems, DeliveryAddress
+from .models import UserDetails, Product, Basket, BasketOrderItems, DeliveryAddress,
 
-# Register your models here.
+admin.site.register(UserDetails)
+admin.site.register(Product)
+admin.site.register(Basket)
+admin.site.register(BasketOrderItems)
+admin.site.register(DeliveryAddress)
