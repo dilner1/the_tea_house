bf6885cc043f9c914e73ce2dd4006537c7ceb5dc
dilner1 <dan.milner871@gmail.com>
2022-07-27 18:51:36 +0000

    Removing max_length from country

--- app/migrations/0011_auto_20220721_1933.py
+++ app/migrations/0011_auto_20220721_1933.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='customerinfo',
             name='country',
-            field=django_countries.fields.CountryField(default='Country', max_length=2),
+            field=django_countries.fields.CountryField(default='Country'),
         ),
         migrations.AddField(
             model_name='customerinfo',
