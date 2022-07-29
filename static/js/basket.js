1fcb086d2ca005416ee788ee24a296f613517135
dilner1 <dan.milner871@gmail.com>
2022-07-29 17:00:36 +0000

    trying to sync with heroku

--- static/js/basket.js
+++ static/js/basket.js
@@ -1,9 +1,15 @@
-console.log('sup')
-
 let add_button = document.getElementById("add-to-basket-button")
 
-add_button.addEventListener("click", amendBasket);
-
-function amendBasket() {
-  alert ("Hello World!");
+for ( i = 0; i < updateButton.length, i++) {
+  updateButton[i].addEventListener('click', function(){
+    var productId = this.dataset.productId
+    var action = this.dataset.action
+    console.log('productId:', productId, 'Action', action)
+  })
 }
+
+// add_button.addEventListener("click", amendBasket);
+
+// function amendBasket() {
+//   alert ("Hello World!");
+// }
