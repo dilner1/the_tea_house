af851497343390fba0d2940460626f547a27b996
dilner1 <dan.milner871@gmail.com>
2022-08-15 22:20:37 +0000

    User now showing in js console correctly

--- static/js/basket.js
+++ static/js/basket.js
@@ -4,9 +4,8 @@ for (i = 0; i < add_button.length; i++) {
   add_button[i].addEventListener("click", function(){
     let itemId = this.dataset.product
     let addItem = this.dataset.action
-    
+
     console.log('item Id:', itemId, 'Action:', addItem)
-    
-    console.log('User:', user)
+    console.log(user)
 });
 }
