5231c1dd86e19a081904e4989152af9bec05a061
dilner1 <dan.milner871@gmail.com>
2022-08-15 21:55:55 +0000

    Add action now displaying in console

--- static/js/basket.js
+++ static/js/basket.js
@@ -4,13 +4,9 @@ for (i = 0; i < add_button.length; i++) {
   add_button[i].addEventListener("click", function(){
     let itemId = this.dataset.product
     let addItem = this.dataset.action
-    console.log('itemId:', itemId, 'addItem:', addItem)
-
-    if (user ===  'AnonymousUser'){
-      console.log('Not logged in')
-    }
-    else {
-      console.log('Request sent')
-    }
+    
+    console.log('item Id:', itemId, 'Action:', addItem)
+    
+    console.log('User:', user)
 });
 }
