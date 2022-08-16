2e21e83f976734a24d85368024c3f267de3b30d3
dilner1 <dan.milner871@gmail.com>
2022-08-16 19:53:50 +0000

    creating updateBasketItems function

--- static/js/basket.js
+++ static/js/basket.js
@@ -5,13 +5,33 @@ for (i = 0; i < add_button.length; i++) {
     let itemId = this.dataset.product
     let addItem = this.dataset.action
 
-    console.log('item Id:', itemId, 'Action:', addItem)
+    console.log('Item Id:', itemId, 'Action:', addItem)
 
     if (user == 'AnonymousUser'){
       console.log('You are not logged in')
     } else {
-      console.log('You are logged in as', user)
+      updateBasketItems(itemId, addItem)
     }
-    
 });
+}
+
+function updateBasketItems(itemId, addItem){
+  console.log('User is authenticate, adding to basket...')
+
+  let url = '/update-basket/'
+
+  fetch(url, {
+    method:'POST',
+    headers:{
+      'Content-Type':'application/json',
+    },
+    body:JSON.stringify({'Item Id': itemId, 'Action': addItem})
+  })
+  .then((respons => {
+    return.response.json();
+  })
+  .then((data) => {
+    console.log('Data:', data)
+  })
+  )
 }
