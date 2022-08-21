2dc74d4b3c21f17b540aacb7b8de94a602147d3d
dilner1 <dan.milner871@gmail.com>
2022-08-21 19:59:00 +0000

    Adding YOUR_DOMIN to createCheckoutSessionView

--- app/views.py
+++ app/views.py
@@ -127,15 +127,20 @@ stripe.api_key = settings.STRIPE_SECRET_KEY
 # Stripe checkout
 class createCheckoutSessionView(View):
     def post(self, request, *args, **kwargs):
+        YOUR_DOMAIN = 'localhost'
         checkout_session = stripe.checkout.Session.create(
-        line_items=[
-            {
-                # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                'price': '{{PRICE_ID}}',
-                'quantity': 1,
-            },
-        ],
-        mode='payment',
-        success_url=YOUR_DOMAIN + '/success.html',
-        cancel_url=YOUR_DOMAIN + '/cancel.html',
-    )
+            payment_method_types = ['card'],
+            line_items=[
+                {
+                    # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                    'price': '{{Product.objects.get(id=itemId)}}',
+                    'quantity': 1,
+                },
+            ],
+            mode='payment',
+            success_url=YOUR_DOMAIN + '/success/',
+            cancel_url=YOUR_DOMAIN + '/cancel/',
+        )
+        return JsonResponse({
+            'id': checkout_session.id
+        })
